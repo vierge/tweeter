e5d59a300040c5cb0fcc7cdc94dddd53f2ef4d1b
vierge <marancholia@gmail.com>
2020-01-30 19:19:43 +0000

    tweet button now turns red when tweet is invalid.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,10 +3,15 @@ $(document).ready(function() {
   $(".new-tweet textarea").on('keyup', function(event) {
     const charCount = $(this).val().length;
     const counter = $(this).siblings('.counter');
+    const button = $(this).siblings('input');
     console.log(charCount);
     if (charCount >= 140) {
+      button.css("background-color", "red");
       counter.css("color","red");
+    } else if (charCount === 0) {
+      button.css("background-color", "red")
     } else {
+      button.css("background-color", "#4056a1");
       counter.css("color","inherit");
     }
     counter.text(`${140 - charCount}`);
