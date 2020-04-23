ac79106375f8a5d640048f6ce53c68e8b26cac39
vierge <marancholia@gmail.com>
2020-04-23 21:11:45 +0000

    css bugfixes. it works and i like it.

--- server/index.js
+++ server/index.js
@@ -33,15 +33,16 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
-  browserSync({
-    open: false,
-    proxy: 'http://localhost:' + PORT,
-    files: ["public/**/*.*"],
-    injectChanges: true,
-    watchOptions: {
-      usePolling: true
-    },
-    notify: true
-  });
+  // BELOW FOR DEV ONLY
+  // browserSync({ 
+  //   open: false,
+  //   proxy: 'http://localhost:' + PORT,
+  //   files: ["public/**/*.*"],
+  //   injectChanges: true,
+  //   watchOptions: {
+  //     usePolling: true
+  //   },
+  //   notify: true
+  // });
   console.log("Example app listening on port " + PORT);
 });
